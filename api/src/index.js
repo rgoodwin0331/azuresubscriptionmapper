47c09ffeaeaec50558c6689a357e35bac8010655
Ryan Goodwin <rgoodwin@thrivenextgen.com>
2026-06-15 16:37:44 -0400

    Account Search Update

--- api/src/index.js
+++ api/src/index.js
@@ -395,7 +395,7 @@ app.http('unknown-subscriptions', {
 app.http('accounts-search', {
   methods: ['GET'],
   authLevel: 'anonymous',
-  route: 'accounts/{accountId:int}',
+  route: 'accounts-search',
   handler: async (request, context) => {
     try {
       const q = (request.query.get('q') || '').trim();
